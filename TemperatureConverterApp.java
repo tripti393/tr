d97aa0ca4088f3a52921eb4c494ab5c21ecdd35e
tripti393 <146345294+tripti393@users.noreply.github.com>
2023-10-14 09:14:15 +0530

    Add files via upload

---
+++ TemperatureConverterApp.java
@@ -0,0 +1,17 @@
+ import java.util.Scanner;
+
+public class TemperatureConverterApp {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter temperature in Celsius: ");
+        double celsius = scanner.nextDouble();
+
+        // Convert Celsius to Fahrenheit
+        double fahrenheit = (celsius * 9/5) + 32;
+
+        System.out.println("Temperature in Fahrenheit: " + fahrenheit);
+
+        scanner.close();
+    }
+}
